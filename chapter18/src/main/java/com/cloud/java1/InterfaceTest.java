65c19547d387106a18287196053bb80621cbeefa
ByondCloud <1095938907@qq.com>
2023-05-06 05:43:20 +0800

    字节码解读

---
+++ chapter18/src/main/java/com/cloud/java1/InterfaceTest.java
@@ -0,0 +1,23 @@
+package com.cloud.java1;
+
+/**
+ * @author ByondCloud
+ * @version 1.0
+ * @Date 2023/5/6
+ * @Time 0:49
+ */
+public class InterfaceTest implements A, B, C{
+    public static void main(String[] args) {
+
+    }
+}
+
+interface A {
+
+}
+interface B {
+
+}
+interface C {
+
+}
